fe7545ce4e8b534634c00044dbd80c4bfa3f28b6
Sylwia Szymańska <sylwiakszymanska@gmail.com>
2020-07-15 13:03:08 +0200

    Add coloring element on click

--- src/Dancefloor.tsx
+++ src/Dancefloor.tsx
@@ -1,15 +1,17 @@
-import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
+import React, { ChangeEvent, MouseEvent, useEffect, useRef, useState } from 'react';
 
+// const serverMockedResponse = () => {
+//
+// }
 
 const Dancefloor = () => {
     const canvas = useRef(null);
-    const colors = ["#3B60FF", "#F1A0C5", "#EEAB1E", "#5CC6F3",
-        "#E4F1FB", "#3353D8", "#FBEBEB", "#C7E0FE"];
+    const [colors, setColors]= useState(["#3B60FF", "#F1A0C5", "#EEAB1E", "#5CC6F3",
+        "#E4F1FB", "#3353D8", "#FBEBEB", "#C7E0FE"]);
     const dimension = 100;
 
     const [rows, setRows] = useState(2);
     const [columns, setColumns] = useState(4);
-
 
     useEffect(() => {
         const ctx = canvas.current.getContext("2d");
@@ -24,17 +26,31 @@ const Dancefloor = () => {
         }
 
         generateTable(rows, columns);
-    }, [rows, columns]);
+    }, [rows, columns, colors]);
 
     const handleRowsChange = (event: ChangeEvent<HTMLInputElement> ) => {
         setRows(parseInt(event.target.value));
     }
-    const handleColumnsChange = (event:  ChangeEvent<HTMLInputElement>) => {
+    const handleColumnsChange = (event: ChangeEvent<HTMLInputElement>) => {
         setColumns(parseInt(event.target.value));
     }
 
     const handleGenerate = () => {}
+    const getClickedTile = (x: number, y: number) => {
+        const diffY = y - canvas.current.offsetTop ;
+        const diffX = x - canvas.current.offsetLeft;
+        const clickedColumn = Math.floor(diffX / dimension);
+        const clickedRow = Math.floor(diffY / dimension);
+        console.log(clickedRow, clickedColumn);
+        const elementInArray = clickedRow * columns + clickedColumn;
+        const newColors = [...colors];
+        newColors[elementInArray] = "#000000"
+        setColors(newColors);
+    }
 
+    const handleCanvasOnClick = (event: MouseEvent<HTMLCanvasElement>) => {
+        getClickedTile( event.clientX, event.clientY);
+    }
 
     return (
         <div className="Dancefloor">
@@ -44,7 +60,7 @@ const Dancefloor = () => {
                 <button type="button" onClick={handleGenerate}>Generate</button>
 
             </div>
-            <canvas className="Dancefloor-canvas" ref={canvas} width={1600} height={400} />
+            <canvas className="Dancefloor-canvas" ref={canvas} width={1600} height={400} onClick={handleCanvasOnClick} />
         </div>
     )
 }
